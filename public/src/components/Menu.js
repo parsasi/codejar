e4148d9f0a2972bd093617f0303ae9fe9a980669
parsasi <parsasi@rocketmail.com>
2020-07-26 02:52:46 +0430

    Logo Is Added To The Menu

--- public/src/components/Menu.js
+++ public/src/components/Menu.js
@@ -2,10 +2,14 @@ import React from 'react'
 import Fa from './Fa'
 import MenuItemLogic from './MenuItemLogic'
 import AddFileButton from './AddFileButton'
+import '../styles/Menu.css'
+import Logo from '../assets/logo_transparent_sq.png'
 export default (props) => {
     return (
         <div className="menu">
-            <span>Work area</span>
+            <div className="MenuLogo">
+                <img src={Logo} alt="Logo"/>
+            </div>
             <MenuItemLogic current={true} save={true} file={{extention : 'css' , name:'styles'}}/>
             <MenuItemLogic current={false} save={false} file={{extention : 'html' , name:'index'}}/>
             <MenuItemLogic current={false} save={true} file={{extention : 'js' , name:'main'}}/>
