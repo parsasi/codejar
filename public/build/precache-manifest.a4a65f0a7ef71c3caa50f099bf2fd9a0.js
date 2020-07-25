eb54f9393e5f8b63451264eed5f89a8ef233c6e5
parsasi <parsasi@rocketmail.com>
2020-07-25 18:12:58 +0430

    Favicon changed

---
+++ public/build/precache-manifest.a4a65f0a7ef71c3caa50f099bf2fd9a0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b35f01a1aaf333119a9e0144324f9a2d",
+    "url": "/css.worker.js"
+  },
+  {
+    "revision": "7f57c206c15e018aa95e390036396cb2",
+    "url": "/editor.worker.js"
+  },
+  {
+    "revision": "eb576b459cb2b8e82837c126c7bab229",
+    "url": "/html.worker.js"
+  },
+  {
+    "revision": "9815a6ab5c46705d8a6860ac219153c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39529cb80173e13ba9478b165afa88ec",
+    "url": "/json.worker.js"
+  },
+  {
+    "revision": "6ecc092ebb5ad1a4256c",
+    "url": "/static/css/main.20fa6035.chunk.css"
+  },
+  {
+    "revision": "03e8c7755b44b0cbcb9f",
+    "url": "/static/js/2.82364fe5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.82364fe5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ecc092ebb5ad1a4256c",
+    "url": "/static/js/main.d54e9a5f.chunk.js"
+  },
+  {
+    "revision": "c9e3c27047e9e3d0892d",
+    "url": "/static/js/runtime-main.f6076a64.js"
+  },
+  {
+    "revision": "90bfce78fbbf4ccf6450c2f6b5458c7f",
+    "url": "/static/media/coming-soon-background.90bfce78.jpg"
+  },
+  {
+    "revision": "4b58afbc97c041391451c3d536bbf6b8",
+    "url": "/static/media/logo_transparent.4b58afbc.png"
+  }
+]);
