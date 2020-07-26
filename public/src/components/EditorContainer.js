396ff7bdfcf163c6fe19bb234f0c9df5121ee9ad
parsasi <parsasi@rocketmail.com>
2020-07-26 17:27:57 +0430

    Status Bar Component Added

--- public/src/components/EditorContainer.js
+++ public/src/components/EditorContainer.js
@@ -3,12 +3,14 @@ import WindowBar from './WindowBar'
 import SideBar from './SideBar'
 import CodeEditor from './CodeEditor'
 import '../styles/generics.css'
+import StatusBar from './StatusBar'
 export default (props) => {
     return (
         <div className="EditorContainer">
             <WindowBar/>
             <SideBar />
             <CodeEditor />
+            <StatusBar />
         </div>
     )
 }
