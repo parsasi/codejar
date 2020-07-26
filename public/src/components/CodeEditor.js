9ff0742f79e0ca9ff5e57e181afdc5ef5e9578e7
parsasi <parsasi@rocketmail.com>
2020-07-26 17:44:51 +0430

    Moncao Added to The Code Component Styles need more work

--- public/src/components/CodeEditor.js
+++ public/src/components/CodeEditor.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import Monaco from './Monaco'
 export default (props) => {
     return (
         <div className="code">
-                
+            <Monaco width={100 + '%'} height={100+'vh'} />
         </div>
     )
 }
