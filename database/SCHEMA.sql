6ae7d6e78bac76d5a3d7928ff63509f06d5ef7ea
parsasi <parsasi@rocketmail.com>
2020-08-02 23:44:10 +0430

    SCHEMA file Added Workspaces table created

---
+++ database/SCHEMA.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS workspaces;
+-- Workspaces have their internal id as well as a public code for the public access and an admin code for admin access link
+CREATE TABLE workspaces (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    public_code VARCHAR(20) NOT NULL UNIQUE,
+    time_created TIMESTAMP NOT NULL DEFAULT  CURRENT_TIMESTAMP,
+    admin_code varchar(255) NOT NULL UNIQUE
+)
