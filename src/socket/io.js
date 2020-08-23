7bc088c226fa62793591b149d575b84ce203ebac
parsasi <parsasi@rocketmail.com>
2020-08-23 19:50:02 +0430

    Prints out the rooms created for an id

--- src/socket/io.js
+++ src/socket/io.js
@@ -2,11 +2,12 @@ module.exports = (io , db) => {
     const getWorkspace = require('../controllers/getWorkspace')(db)
     require('dotenv').config()
     io.on('connection', (socket) => {
+        console.log(io.sockets.adapter.rooms)
         const workspaceId = socket.handshake.query.workspaceId
         getWorkspace(workspaceId)
         .then(results => {
-            const isAdmin = workspaceId === results[0].admin_code
-            
+            const adminCode = results[0].admin_code
+            socket.join(adminCode)
         })
         .catch(console.log)
     });
