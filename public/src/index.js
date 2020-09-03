2354652b4376bfbcf52fad6e3667def6036e47c5
parsasi <parsasi@rocketmail.com>
2020-09-03 22:55:07 +0430

    Sentry is setup for tracing errors Resolves #20

--- public/src/index.js
+++ public/src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
+
+
+
+//Not sure if the dsn is a secret key, as it's not stated as so, in Sentry docs
+Sentry.init({
+  dsn: "https://073c6ef986ac4a31a2802985cc807fb3@o349957.ingest.sentry.io/5416890",
+  integrations: [
+    new Integrations.BrowserTracing(),
+  ],
+  tracesSampleRate: 1,
+});
+
 
 ReactDOM.render(
   <React.StrictMode>
