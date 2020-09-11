a37c7c81b9669e0c03570173b7b648f2ba3228d9
parsasi <parsasi@rocketmail.com>
2020-09-11 15:31:40 +0430

    serverRoute helper uses enviroment variables now

--- public/src/helpers/getServerRoute.js
+++ public/src/helpers/getServerRoute.js
@@ -1,3 +1,3 @@
 export default () => {
-    return process.env.NODE_ENV === 'development' ? 'http://localhost:8080/' : 'http://codejar.live/'
+    return process.env.NODE_ENV === 'development' ? process.env["REACT_APP_DEVELOPMENT_SERVER_URL"] : process.env["REACT_APP_PRODUCTION_SERVER_URL"]
 }
